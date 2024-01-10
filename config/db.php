5e13fe84108e338493b7dbfa9b07f45f9a1428e2
M4g4s <bohdanmahas.work@gmail.com>
2024-01-10 15:17:23 +0100

    Creating user "todo" in database + creating a migration

--- config/db.php
+++ config/db.php
@@ -2,10 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'dsn' => 'mysql:host=localhost;dbname=todo',
+    'username' => 'todo',
+    'password' => 'Lp(K]bw9CYKDf]Li',
+    'charset' => 'utf8mb4', // utf8mb4 for using emojis in todo list
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
